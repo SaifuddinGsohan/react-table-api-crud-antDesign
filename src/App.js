d05abc80dce5f9128dfdf67deaa780b90fe1c6ee
Saifuddin sohan <32895869+SaifuddinGsohan@users.noreply.github.com>
2023-01-01 14:40:40 +0600

    Table task 1
    Table  task 1

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import { Button, Col, Row } from "antd";
+import React, { useState, useEffect } from "react";
+import { AutoComplete } from "antd";
+
+import { getAllProducts } from "./API";
+import { List } from "./component/List";
+
+export const App = () => {
+  //Search
+  const [options, setOptions] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [card, setCard] = useState(true);
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    getAllProducts().then((res) => {
+      setProducts(res.products);
+    });
+  }, []);
+
+  const onSearch = () => {
+    getAllProducts().then((res) => {
+      setOptions(
+        res?.products?.map((m) => ({ label: m.title, value: m.title }))
+      );
+    });
+  };
+
+  const onSelect = (data) => {
+    setList((oldArray) => [
+      ...oldArray,
+      products
+        ?.map((m) => ({
+          ...m,
+          quantity: 1,
+          total: m.price,
+          discountedPrice: Math.round(
+            m.price - m.discountPercentage * (100 / m.price)
+          ),
+        }))
+        ?.find((f) => f.title === data),
+    ]);
+  };
+
+  //Table
+
+  console.log("List - ", list);
+  //
+
+  return (
+    <>
+      <Row>
+        <Col span={8}>
+          <AutoComplete
+            options={options}
+            style={{
+              width: 200,
+            }}
+            filterOption={true}
+            onSelect={onSelect}
+            onSearch={onSearch}
+            placeholder="input here"
+          />
+          <Button onClick={() => setCard(!card)} type="primary">
+            Curd
+          </Button>
+        </Col>
+
+        <Col span={16}>
+          {/* {card ? <List data={list} /> : <></>} */}
+          {list?.length > 0 && <List list={list} setList={setList} />}
+          {/* {list?.map((i, k) => {
+            return <h2>{i.title}</h2>;
+          })} */}
+          <Button onClick={() => {}} type="primary">
+            Checkout
+          </Button>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default App;
---
+++ src/component/List.js
@@ -0,0 +1,74 @@
+import { DeleteOutlined } from "@ant-design/icons";
+import {  InputNumber, Table} from "antd";
+
+export const List = ({ list, setList }) => {
+  return (
+    <>
+      <Table
+        columns={[
+          {
+            title: "Title",
+            dataIndex: "title",
+          },
+          {
+            title: "Price",
+            dataIndex: "price",
+            render: (value) => {
+              return <span>${value}</span>;
+            },
+          },
+          {
+            title: "Quantity",
+            dataIndex: "quantity",
+            render: (value, record) => {
+              return (
+                <InputNumber
+                  min={0}
+                  defaultValue={value}
+                  onChange={(value) => {
+                    setList((p) =>
+                      p.map((cart) => {
+                        if (record.id === cart.id) {
+                          cart.total = cart.price * value;
+                        }
+                        return cart;
+                      })
+                    );
+                  }}
+                ></InputNumber>
+              );
+            },
+          },
+          {
+            title: "Total",
+            dataIndex: "total",
+            render: (value) => {
+              return <span>${value}</span>;
+            },
+          },
+          {
+            title: "Delate",
+            dataIndex: "delate",
+            render: (value, record) => (
+              <DeleteOutlined
+                style={{ fontSize: "20px", color: "#c11c" }}
+                onClick={() => {
+                  setList((p) =>
+                    p.filter((f)=> f.id !== record.id)
+                    )
+                }}
+              />
+            ),
+          },
+        ]}
+        dataSource={list}
+        summary={(data) => {
+          const total = data.reduce((pre, current) => {
+            return pre + current.total;
+          }, 0);
+          return <span>Total: ${total}</span>;
+        }}
+      />
+    </>
+  );
+};
